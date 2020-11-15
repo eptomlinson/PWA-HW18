294d691c0e7ffdaaa4853405209cbd6439d15357
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-15 17:45:33 -0600

    addEventListener added, with install, activate and fetch included

--- public/service-worker.js
+++ public/service-worker.js
@@ -6,10 +6,57 @@ const FILES_TO_CACHE = [
 ];
 
 // globals 
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
 
 // event listener 
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
 
 // activate handler to clean old caches
+self.addEventListener('activate', (event) => {
+    const currentCaches = [PRECACHE, RUNTIME];
+    event.waitUntil(
+      caches
+        .keys()
+        .then((cacheNames) => {
+          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
+        })
+        .then((cachesToDelete) => {
+          return Promise.all(
+            cachesToDelete.map((cacheToDelete) => {
+              return caches.delete(cacheToDelete);
+            })
+          );
+        })
+        .then(() => self.clients.claim())
+    );
+  });
 
 // handler to fetch 
     // return result 
+    self.addEventListener('fetch', (event) => {
+        if (event.request.url.startsWith(self.location.origin)) {
+          event.respondWith(
+            caches.match(event.request).then((cachedResponse) => {
+              if (cachedResponse) {
+                return cachedResponse;
+              }
+      
+              return caches.open(RUNTIME).then((cache) => {
+                return fetch(event.request).then((response) => {
+                  return cache.put(event.request, response.clone()).then(() => {
+                    return response;
+                  });
+                });
+              });
+            })
+          );
+        }
+      });
