07f51a3be61b8c84f61f015836b7b965455ba939
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-15 17:46:16 -0600

    service worker file formatted

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,51 +12,51 @@ const RUNTIME = 'runtime';
 // event listener 
 self.addEventListener('install', (event) => {
     event.waitUntil(
-      caches
-        .open(PRECACHE)
-        .then((cache) => cache.addAll(FILES_TO_CACHE))
-        .then(self.skipWaiting())
+        caches
+            .open(PRECACHE)
+            .then((cache) => cache.addAll(FILES_TO_CACHE))
+            .then(self.skipWaiting())
     );
-  });
+});
 
 // activate handler to clean old caches
 self.addEventListener('activate', (event) => {
     const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
-        })
-        .then((cachesToDelete) => {
-          return Promise.all(
-            cachesToDelete.map((cacheToDelete) => {
-              return caches.delete(cacheToDelete);
+        caches
+            .keys()
+            .then((cacheNames) => {
+                return cacheNames.filter((cacheName) => !currentCaches.includes(cacheName));
             })
-          );
-        })
-        .then(() => self.clients.claim())
+            .then((cachesToDelete) => {
+                return Promise.all(
+                    cachesToDelete.map((cacheToDelete) => {
+                        return caches.delete(cacheToDelete);
+                    })
+                );
+            })
+            .then(() => self.clients.claim())
     );
-  });
+});
 
 // handler to fetch 
-    // return result 
-    self.addEventListener('fetch', (event) => {
-        if (event.request.url.startsWith(self.location.origin)) {
-          event.respondWith(
+// return result 
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
             caches.match(event.request).then((cachedResponse) => {
-              if (cachedResponse) {
-                return cachedResponse;
-              }
-      
-              return caches.open(RUNTIME).then((cache) => {
-                return fetch(event.request).then((response) => {
-                  return cache.put(event.request, response.clone()).then(() => {
-                    return response;
-                  });
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then((cache) => {
+                    return fetch(event.request).then((response) => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
                 });
-              });
             })
-          );
-        }
-      });
+        );
+    }
+});
