9a0adc171bb55707066f9c9ec33a67b4c3eccde2
Erik Tomlinson <eptomlinson@gmail.com>
2020-11-16 09:59:54 -0600

    added files to cache in service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,6 +3,9 @@ const FILES_TO_CACHE = [
     '/',
     '/index.html',
     '/public/style.css',
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
 ];
 
 // globals 
